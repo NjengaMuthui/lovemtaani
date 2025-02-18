4cd7dd97de7abdd7f21d6fc28083bc46e456a940
Njenga <petermuthuinjenga@gmail.com>
2025-02-18 11:39:00 +0300

    redeploy

--- components/gallery.vue
+++ components/gallery.vue
@@ -33,6 +33,7 @@ import slide2 from "~/assets/slide2.jpg";
 import slide3 from "~/assets/slide3.jpg";
 import slide4 from "~/assets/slide4.jpg";
 import slide5 from "~/assets/slide5.jpg";
+import gal1 from "~/assets/gal1.jpg";
 
 const images = ref([slide, slide2, slide3, slide4, slide5]);
 
